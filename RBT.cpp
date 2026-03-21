6519252272cc9bf5937099904ed76222146346f3
neSux0 <senxu98@gmail.com>
2026-03-21 06:20:46 -0400

    insert function created

--- RBT.cpp
+++ RBT.cpp
@@ -63,3 +63,46 @@ size_t RBT::size() const {
 bool RBT::empty() const {
     return nodeCount == 0;
 }
+
+
+void RBT::insert(const string& key, const FoodItem* food) {
+    if (food == nullptr) return;
+
+    string lowerKey = lowerCase(key);
+
+    Node* parent = nullptr;
+    Node* current = root;
+
+    while (current != nullptr) {
+        parent = current;
+
+        if (lowerKey == current->key) {
+            //vector is used to make a fair comparison with hashtable.
+            //also some keys with same name but diff info may repeat in the large dataset, so vector used to be safe.
+            current->values.push_back(food);
+            return;
+        }
+        else if (lowerKey < current->key) {
+            current = current->left;
+        }
+        else {
+            current = current->right;
+        }
+    }
+
+    Node* newNode = new Node(lowerKey, food);
+    newNode->parent = parent;
+
+    if (parent == nullptr) {
+        root = newNode;
+    }
+    else if (lowerKey < parent->key) {
+        parent->left = newNode;
+    }
+    else {
+        parent->right = newNode;
+    }
+
+    fixInsert(newNode);
+    nodeCount++;
+}
